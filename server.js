a32e4cf81c734f900ca9d691ef655c3b7d3ae721
StefanT2024 <stefantujarov24@gmail.com>
2026-02-17 15:14:07 -0600

    added admin login page

--- server.js
+++ server.js
@@ -2,9 +2,15 @@ const express = require('express');
 const app = express();
 const path = require('path');
 app.use(express.urlencoded({extended: true}));
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3001
+
+app.get('/studentLogin',(req, res) => {res.sendFile(path.join(__dirname,'htmlFiles/student/loginPage.html'));});
+app.post('/submitStudentLogin',(req,res) => {res.send("Submitted student email: " + req.body.studentEmail + " Submitted student pass: " + req.body.studentPassword); console.log(req.body);});
+
+app.get('/adminLogin',(req, res) => {res.sendFile(path.join(__dirname,'htmlFiles/ admin/adminLoginPage.html'));});
+app.post('/submitAdminLogin',(req,res) => {res.send("Submitted email: " + req.body.adminEmail + " submitted password: " + req.body.adminPassword); console.log(req.body);});
+
 app.listen(PORT, () =>{
     console.log('Server running on port', PORT);
 });
-app.get('/studentLogin',(req, res) => {res.sendFile(path.join(__dirname,'htmlFiles/student/loginPage.html'));});
-app.post('/submitStudentLogin',(req,res) => {res.send("Submitted student email: " + req.body.studentEmail + " Submitted student pass: " + req.body.studentPassword); console.log(req.body);});
+

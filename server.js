cc779493b27b380e060dbe616d0564e4ad264a9d
Dillon Opperman <dillonopp@gmail.com>
2026-02-19 16:46:17 -0600

    Add tutor page and update server

--- server.js
+++ server.js
@@ -7,8 +7,12 @@ const PORT = process.env.PORT || 3001
 app.get('/studentLogin',(req, res) => {res.sendFile(path.join(__dirname,'htmlFiles/student/loginPage.html'));});
 app.post('/submitStudentLogin',(req,res) => {res.send("Submitted student email: " + req.body.studentEmail + " Submitted student pass: " + req.body.studentPassword); console.log(req.body);});
 
-app.get('/adminLogin',(req, res) => {res.sendFile(path.join(__dirname,'htmlFiles/ admin/adminLoginPage.html'));});
-app.post('/submitAdminLogin',(req,res) => {res.send("Submitted email: " + req.body.adminEmail + " submitted password: " + req.body.adminPassword); console.log(req.body);});
+app.get('/adminLogin',(req, res) => {res.sendFile(path.join(__dirname,'htmlFiles/admin/adminLoginPage.html'));});
+app.post('/submitAdminLogin',(req,res) => {res.send("Submitted email: " + req.body.adminEmail + " submitted password: " + req.body.adminPassword); console.log(req.body);} );
+
+// Tutor routes
+app.get('/tutorLogin',(req, res) => {res.sendFile(path.join(__dirname,'htmlFiles/tutuor/TutorPage.html'));});
+app.post('/submitTutorLogin',(req,res) => {res.send("Submitted tutor email: " + req.body.tutorEmail + " Submitted tutor pass: " + req.body.tutorPassword); console.log(req.body);} );
 
 app.listen(PORT, () =>{
     console.log('Server running on port', PORT);

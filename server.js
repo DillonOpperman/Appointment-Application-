51d740b39be6c6519e9cd57b18a0840746f251c6
StefanT2024 <stefantujarov24@gmail.com>
2026-02-23 12:04:56 -0600

    finished basic layout of admin Dashboard, html, css

--- server.js
+++ server.js
@@ -4,8 +4,14 @@ const path = require('path');
 app.use(express.urlencoded({extended: true}));
 const PORT = process.env.PORT || 3001
 
+app.use('/cssFiles', express.static(path.join(__dirname, 'cssFiles')));
+
 app.get('/studentLogin',(req, res) => {res.sendFile(path.join(__dirname,'htmlFiles/student/loginPage.html'));});
 app.post('/submitStudentLogin',(req,res) => {res.send("Submitted student email: " + req.body.studentEmail + " Submitted student pass: " + req.body.studentPassword); console.log(req.body);});
+
+app.get('/adminDashboard', (req, res) => {
+    res.sendFile(path.join(__dirname, 'htmlFiles/ admin/adminDashboard.html'));
+});
 
 app.get('/adminLogin',(req, res) => {res.sendFile(path.join(__dirname,'htmlFiles/ admin/adminLoginPage.html'));});
 app.post('/submitAdminLogin',(req,res) => {res.send("Submitted email: " + req.body.adminEmail + " submitted password: " + req.body.adminPassword); console.log(req.body);});

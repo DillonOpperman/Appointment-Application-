4951bee736aa837a7abfdbbc4321acefc70aa6e8
YellowCardinal <129400130+YellowCardinal@users.noreply.github.com>
2026-03-18 16:24:43 -0500

    My commit

--- Servers/Controller/studentController.js
+++ Servers/Controller/studentController.js
@@ -6,14 +6,17 @@ exports.showLogin = (req, res) => {
     res.render('Student/studentLoginPage')
 };
 exports.submitLogin = (req, res) => {
-    
+    res.redirect("/studentHome", {
+        //studentEmail: req.body.studentEmail,
+        //studentPassword: req.body.studentPassword
+    });
 }
 exports.showHome = (req, res) => {
     //res.send("Submitted student email: " + req.body.studentEmail + " Submitted student pass: " + req.body.studentPassword);
 
-    res.redirect("Student/studentHome", {
-        studentEmail: req.body.studentEmail,
-        studentPassword: req.body.studentPassword
+    res.render("Student/studentHome", {
+        //studentEmail: req.body.studentEmail,
+        //studentPassword: req.body.studentPassword
     });
 };
 exports.createAppointment = (req, res) => {
